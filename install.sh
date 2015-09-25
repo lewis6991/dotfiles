096be8ad6e3c3ebd4ee0e3839eafe96496ab0d2b
Lewis Russell <lewis.russell@arm.com>
2015-09-25 08:49:47 +0100

    Moved plugins to use pathogen. added powerline fonts.

--- install.sh
+++ install.sh
@@ -55,10 +55,19 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Set up SystemVerilog/UVM syntax
-git clone git://github.com/WeiChungWu/vim-SystemVerilog.git ~/.vim/bundle/vim-SystemVerilog.git
+git clone git://github.com/WeiChungWu/vim-SystemVerilog.git ~/.vim/bundle/vim-SystemVerilog
 
 # Set up supertab (tab completion)
 git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/supertab
+
+# Set up tabular
+git clone git://github.com/godlygeek/tabular.git ~/.vim/bundle/tabular
+
+# Set up vim-fugitive
+git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+
+# Set up vim airline
+git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
 
 # Set up bash syntax
 wget http://www.panix.com/~elflord/vim/syntax/bash.vim
@@ -66,6 +75,15 @@ wget http://ftp.vim.org/vim/runtime/syntax/awk.vim
 mkdir ~/.vim/syntax
 mv bash.vim ~/.vim/syntax/
 mv awk.vim ~/.vim/syntax/
+
+# Set up powerline fonts
+pushd ~
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install
+cd
+rm -rf fonts
+popd
 
 # Set up vimrc
 cp -r vimrc ~/.vimrc
