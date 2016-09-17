cec082496ef8d8c7e858461ddc2d463707c027ab
lewis6991 <lewis6991@gmail.com>
2016-09-17 18:06:53 +0100

    Reverted install.sh overwrite.

--- install.sh
+++ install.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+readonly   RED='\033[0;31m'
+readonly GREEN='\033[0;32m'
+readonly  CYAN='\033[0;36m'
+readonly  GREY='\033[1;30m'
+readonly   MAG='\033[0;34m'
+readonly    NC='\033[0m' # No Color
+
+function echo_error {
+    echo -e "${RED}Error:" $@ "$NC"
+}
+
+function echo_ok {
+    echo -e ${GREEN}OK${NC}
+}
+
 function check_cmd {
     echo -n Checking $1 is installed...
     if which $1 >/dev/null; then
-        echo OK
+        echo_ok
         return 0
     else
         echo No
@@ -12,7 +27,7 @@ function check_cmd {
             brew install $1
             return 0
         else
-            echo Error: $1 is not installed.
+            echo_error $1 is not installed.
             return 1
         fi
     fi
@@ -31,40 +46,78 @@ function link_file {
     fi
 }
 
-INSTALL_FONTS=$1
-INSTALL_VIM=$2
+function check_dependencies {
+    # Check that required commands are installed
+    if ! check_cmd git  ; then exit; fi
+    if ! check_cmd rsync; then exit; fi
+    if ! check_cmd wget ; then exit; fi
+    if ! check_cmd curl ; then exit; fi
+}
 
-# Check that required commands are installed
-if ! check_cmd git  ; then exit; fi
-if ! check_cmd rsync; then exit; fi
-if ! check_cmd wget ; then exit; fi
-if ! check_cmd curl ; then exit; fi
-
-source git_config
-
-if [ $INSTALL_VIM -ne 0 ]; then
+function install_vim {
     rm -rf ~/.vim
     link_file vimrc ~/.vimrc
     vim +PlugInstall +qall
-fi
+}
 
-# # Set up config files
-link_file agignore  ~/.agignore
-link_file bashrc    ~/.bashrc
-link_file bashrc    ~/.bash_profile
-link_file tmux.conf ~/.tmux.conf
+function install_dotfiles {
+    link_file tmux.conf ~/.tmux.conf
 
-if [ $INSTALL_FONTS -ne 0 ]; then
-    # Set up powerline fonts
-    pushd ~
-    rm -rf fonts
-    rm -rf .fonts
-    rm -rf .fontconfig
-    git clone https://github.com/powerline/fonts.git
-    cd fonts
-    ./install.sh
-    cd
-    rm -rf fonts
-    popd
-fi
+    if [ "$(uname)" == "Darwin" ]; then
+        link_file bashrc ~/.bash_profile
+    else
+        link_file bashrc ~/.bashrc
+    fi
+}
 
+function install_prompt {
+    mkdir -p $HOME/git
+    cd $HOME/git
+    if [ -d "$HOME/git/fancy-prompt" ]; then
+        echo -e "${CYAN}fancy-prompt already exists${NC}"
+    else
+        git clone https://github.com/lewis6991/fancy-prompt
+        if [ $? -eq 1 ]; then
+            echo_error "Could no install fancy-prompt"
+        fi
+    fi
+    cd -
+}
+
+function install_powerline_fonts {
+    echo -n "Checking if Powerline fonts are installed..."
+    if [ -d "$HOME/.fonts" ]; then
+        POWERLINE_FONTS=$(ls ~/.fonts | grep Powerline | wc -l)
+        if [ "$POWERLINE_FONTS" -gt "0" ]; then
+            INSTALL_FONTS=0
+        else
+            INSTALL_FONTS=1
+        fi
+    else
+        INSTALL_FONTS=1
+    fi
+
+    if [ $INSTALL_FONTS -eq 1 ]; then
+        echo "No"
+        echo "Installing Powerline fonts..."
+        pushd ~
+        rm -rf fonts
+        rm -rf .fonts
+        rm -rf .fontconfig
+        git clone https://github.com/powerline/fonts.git
+        cd fonts
+        ./install.sh
+        cd
+        rm -rf fonts
+        popd
+    else
+        echo_ok
+    fi
+}
+
+check_dependencies
+source git_config
+install_vim
+install_dotfiles
+install_prompt
+install_powerline_fonts
