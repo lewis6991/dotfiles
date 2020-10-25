6c38c743b7d33c13a2f45cf33c8e6c36607e3486
Lewis Russell <lewis6991@gmail.com>
2020-10-25 12:00:53 +0000

    nvim: Enable treesitter folds

--- nvim/lua/treesitter.lua
+++ nvim/lua/treesitter.lua
@@ -1,15 +1,31 @@
+local langs = {
+  "scala",
+  "python",
+  "yaml",
+  "json",
+  "html",
+  "bash",
+  "markdown",
+  "lua",
+  "rst"
+}
+
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {
-      "scala",
-      "python",
-      "yaml",
-      "json",
-      "html",
-      "bash",
-      "markdown",
-      "rst"
-  },
+  ensure_installed = langs,
   highlight = {
     enable = true,
   },
+  indent = {
+    enable = true,
+  },
+
 }
+
+for _, l in pairs(langs) do
+  vim.cmd(
+    'autocmd vimrc FileType '..l..
+    ' set'..
+    ' foldmethod=expr'..
+    ' foldexpr=nvim_treesitter#foldexpr()'
+  )
+end
