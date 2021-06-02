17295d04ef3cc75093a2dab136db000a3e90d27b
Lewis Russell <lewis6991@gmail.com>
2021-06-02 13:44:24 +0100

    Update compe config

--- nvim/lua/lewis6991/plugins.lua
+++ nvim/lua/lewis6991/plugins.lua
@@ -184,6 +184,8 @@ local init = {
   {'hrsh7th/nvim-compe',
     requires = {'andersevenrud/compe-tmux'},
     config = function()
+      local api, fn = vim.api, vim.fn
+
       require'compe'.setup {
         source = {
           path       = true;
@@ -196,37 +198,37 @@ local init = {
       }
 
       local function t(str)
-        return vim.api.nvim_replace_termcodes(str, true, true, true)
+        return api.nvim_replace_termcodes(str, true, true, true)
       end
 
       local check_back_space = function()
-          local col = vim.fn.col('.') - 1
-          if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-              return true
-          else
-              return false
-          end
+        local col = fn.col('.') - 1
+        if col == 0 or fn.getline('.'):sub(col, col):match('%s') then
+          return true
+        else
+          return false
+        end
       end
 
       -- Use (s-)tab to:
       -- move to prev/next item in completion menuone
       _G.tab_complete = function()
-        return vim.fn.pumvisible() == 1 and t'<C-n>'
-        or     check_back_space()       and t'<Tab>'
-        or     vim.fn['compe#complete']()
+        return fn.pumvisible() == 1 and t'<C-n>'
+        or     check_back_space()   and t'<Tab>'
+        or     fn['compe#complete']()
       end
 
-      _G.s_tab_complete = function()
-        return vim.fn.pumvisible() == 1 and t'<C-p>' or t'<S-Tab>'
+      local function keymap(m, k, e, opts)
+        api.nvim_set_keymap(m, k, e, {expr = true, unpack(opts or {})})
       end
 
-      vim.api.nvim_set_keymap('i', '<Tab>'  , 'v:lua.tab_complete()'  , {expr = true})
-      vim.api.nvim_set_keymap('s', '<S-Tab>', 'v:lua.s_tab_complete()', {expr = true})
-      vim.api.nvim_set_keymap('i', '<cr>'   , "compe#confirm('<CR>')" , {expr = true, silent = true, noremap = true})
+      keymap('i', '<Tab>'  , 'v:lua.tab_complete()'  )
+      keymap('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"')
+      keymap('i', '<cr>'   , "compe#confirm('<CR>')" , {silent = true, noremap = true})
 
       -- Workaround for https://github.com/hrsh7th/nvim-compe/issues/329
-      vim.api.nvim_set_keymap('i', '<C-y>', 'pumvisible() ? "\\<C-y>\\<C-y>" : "\\<C-y>"', {expr = true} )
-      vim.api.nvim_set_keymap('i', '<C-e>', 'pumvisible() ? "\\<C-y>\\<C-e>" : "\\<C-e>"', {expr = true} )
+      keymap('i', '<C-y>', 'pumvisible() ? "\\<C-y>\\<C-y>" : "\\<C-y>"')
+      keymap('i', '<C-e>', 'pumvisible() ? "\\<C-y>\\<C-e>" : "\\<C-e>"')
     end
   },
 
