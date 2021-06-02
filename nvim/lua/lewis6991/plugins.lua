752a2c03c05455ce33230869e178c8c5792080e0
Lewis Russell <lewis6991@gmail.com>
2021-06-02 09:22:15 +0100

    Switch from vim-tmux-navigator to Navigator.nvim

--- nvim/lua/lewis6991/plugins.lua
+++ nvim/lua/lewis6991/plugins.lua
@@ -71,7 +71,21 @@ local init = {
   },
 
   'dietsche/vim-lastplace',
-  'christoomey/vim-tmux-navigator',
+
+  -- 'christoomey/vim-tmux-navigator',
+  {'numToStr/Navigator.nvim',
+    config = function()
+      require('Navigator').setup()
+      local map = vim.api.nvim_set_keymap
+      local opts = { noremap = true, silent = true }
+
+      map('n', "<C-h>", "<cmd>lua require('Navigator').left()<CR>" , opts)
+      map('n', "<C-k>", "<cmd>lua require('Navigator').up()<CR>"   , opts)
+      map('n', "<C-l>", "<cmd>lua require('Navigator').right()<CR>", opts)
+      map('n', "<C-j>", "<cmd>lua require('Navigator').down()<CR>" , opts)
+    end
+  },
+
   'ryanoasis/vim-devicons',
   'powerman/vim-plugin-AnsiEsc',
 
