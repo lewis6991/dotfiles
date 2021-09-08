351843fb60a81cffee9dda056871712c91c9ef95
Lewis Russell <lewis6991@gmail.com>
2021-09-08 12:43:40 +0100

    nvim: update null-ls config

--- nvim/lua/lewis6991/null-ls.lua
+++ nvim/lua/lewis6991/null-ls.lua
@@ -1,35 +1,6 @@
 local null_ls = require("null-ls")
 local null_ls_helpers = require("null-ls.helpers")
 
-null_ls.setup {
-  sources = {
-    null_ls.builtins.diagnostics.teal,
-    null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.code_actions.gitsigns,
-  },
-  on_attach = function(_, bufnr)
-    local keymap = function(key, result)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', key, '<cmd>lua '..result..'<CR>',
-        {noremap = true, silent = true})
-    end
-
-    -- keymap('<C-]>'     , 'vim.lsp.buf.definition()')
-    -- keymap('K'         , 'vim.lsp.buf.hover()')
-    -- keymap('gK'        , 'vim.lsp.buf.signature_help()')
-    -- keymap('<C-s>'     , 'vim.lsp.buf.signature_help()')
-    -- keymap('gr'        , 'vim.lsp.buf.references()')
-    -- keymap('<leader>rn', 'vim.lsp.buf.rename()')
-    keymap('<leader>ca', 'vim.lsp.buf.code_action()')
-    keymap('<leader>e' , 'vim.lsp.diagnostic.show_line_diagnostics()')
-    keymap(']d'        , 'vim.lsp.diagnostic.goto_next()')
-    keymap('[d'        , 'vim.lsp.diagnostic.goto_prev()')
-    keymap('go'        , 'vim.lsp.diagnostic.set_loclist()')
-
-
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-  end
-}
-
 local jenkins_lint = {
   method = null_ls.methods.DIAGNOSTICS,
   filetypes = { "Jenkinsfile" },
@@ -60,42 +31,6 @@ local jenkins_lint = {
   }
 }
 
-local pylint_lint = {
-  method = null_ls.methods.DIAGNOSTICS,
-  filetypes = { "python" },
-  generator = null_ls_helpers.generator_factory {
-    command = "pylint",
-    args = {"--output-format=json", '--from-stdin', '$FILENAME'},
-    to_stdin = true,
-    format = 'json',
-    check_exit_code = function()
-      return true
-    end,
-    on_output = function(params)
-      local diagnostics = {}
-      for _, d in ipairs(params.output) do
-        diagnostics[#diagnostics+1] = {
-          source = 'pylint',
-          row = d.line,
-          col = d.column - 1,
-          message = string.format('[%s] %s', d.symbol, d.message),
-          code = d['message-id'],
-          severity = d.type == "error" and 1
-                  or d.type == "fatal" and 1
-                  or d.type == "warning" and 2
-                  or d.type == "informational" and 3
-                  or d.type == "refactor" and 3
-                  or d.type == "convention" and 4,
-        }
-        -- "module": "fts.colors",
-        -- "obj": "",
-        -- "path": "fts/colors.py",
-      end
-      return diagnostics
-    end,
-  },
-}
-
 local keymap = function(bufnr, key, result)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', key, result, {noremap = true})
 end
@@ -110,8 +45,10 @@ null_ls.setup {
   sources = {
     null_ls.builtins.diagnostics.teal,
     null_ls.builtins.diagnostics.shellcheck,
+    null_ls.builtins.diagnostics.pylint,
+    null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.diagnostics.luacheck,
     -- null_ls.builtins.code_actions.gitsigns,
     jenkins_lint,
-    pylint_lint,
   }
 }
