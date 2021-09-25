e67fcfa2963a0a4e92fcfc59eadb456f688226b8
Lewis Russell <lewis6991@gmail.com>
2021-09-25 10:16:01 +0100

    nvim: null-ls changes

--- nvim/lua/lewis6991/null-ls.lua
+++ nvim/lua/lewis6991/null-ls.lua
@@ -31,24 +31,35 @@ local jenkins_lint = {
   }
 }
 
-local keymap = function(bufnr, key, result)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', key, result, {noremap = true})
-end
-
 null_ls.setup {
-  on_attach = function(_, bufnr)
-    keymap(bufnr, ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
-    keymap(bufnr, '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
-    keymap(bufnr, 'go', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>')
-  end,
-  diagnostics_format = "#{s}: #{m}",
   sources = {
-    null_ls.builtins.diagnostics.teal,
+    -- null_ls.builtins.diagnostics.teal,
     null_ls.builtins.diagnostics.shellcheck,
     null_ls.builtins.diagnostics.pylint,
     null_ls.builtins.diagnostics.flake8,
     -- null_ls.builtins.diagnostics.luacheck,
     -- null_ls.builtins.code_actions.gitsigns,
     jenkins_lint,
-  }
+  },
+  diagnostics_format = "#{s}: #{m}",
+  on_attach = function(_, bufnr)
+    local keymap = function(key, result)
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', key, '<cmd>lua '..result..'<CR>',
+        {noremap = true, silent = true})
+    end
+
+    -- keymap('<C-]>'     , 'vim.lsp.buf.definition()')
+    -- keymap('K'         , 'vim.lsp.buf.hover()')
+    -- keymap('gK'        , 'vim.lsp.buf.signature_help()')
+    -- keymap('<C-s>'     , 'vim.lsp.buf.signature_help()')
+    -- keymap('gr'        , 'vim.lsp.buf.references()')
+    -- keymap('<leader>rn', 'vim.lsp.buf.rename()')
+    keymap('<leader>ca', 'vim.lsp.buf.code_action()')
+    keymap('<leader>e' , 'vim.lsp.diagnostic.show_line_diagnostics()')
+    keymap(']d'        , 'vim.lsp.diagnostic.goto_next()')
+    keymap('[d'        , 'vim.lsp.diagnostic.goto_prev()')
+    keymap('go'        , 'vim.lsp.diagnostic.set_loclist()')
+
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  end
 }
