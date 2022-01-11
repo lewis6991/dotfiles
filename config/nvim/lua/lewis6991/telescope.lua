871cd04f56b600c48515d232cbc89bb81b76fca9
Lewis Russell <lewis6991@gmail.com>
2022-01-11 12:00:15 +0000

    nvim: lazy load telescope

--- config/nvim/lua/lewis6991/telescope.lua
+++ config/nvim/lua/lewis6991/telescope.lua
@@ -1,30 +1,36 @@
 
-require'telescope'.setup {
-  defaults = {
-    preview = false,
-    layout_config = {
-      preview_cutoff = 20,
+local function setup()
+  require'telescope'.setup {
+    defaults = {
+      preview = false,
+      layout_config = {
+        preview_cutoff = 20,
+      },
+      selection_strategy = "reset",
+      winblend = 15,
     },
-    selection_strategy = "reset",
-    winblend = 15,
-  },
-  extensions = {
-    fzf = {
-      override_generic_sorter = false, -- Causes crashes
-    },
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown{}
+    extensions = {
+      fzf = {
+        override_generic_sorter = false, -- Causes crashes
+      },
+      ["ui-select"] = {
+        require("telescope.themes").get_dropdown{}
+      }
     }
   }
-}
+  require('telescope').load_extension('fzf')
+  require('telescope').load_extension('ui-select')
+  setup = nil
+end
 
-require('telescope').load_extension('fzf')
-require('telescope').load_extension('ui-select')
 
 local keymap = function(key, fun, opts)
   vim.api.nvim_set_keymap('n', key, '', {
     desc = 'Telescope '..fun..vim.inspect(opts or '', {newline='', indent=''}),
     callback = function()
+      if setup then
+        setup()
+      end
       require('telescope.builtin')[fun](opts)
     end,
     noremap = true,
