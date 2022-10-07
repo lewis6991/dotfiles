e0626e9247600eb4e209236204023acc26df060f
Lewis Russell <lewis6991@gmail.com>
2022-10-07 11:14:38 +0100

    nvim: update treesitter

--- config/nvim/lua/lewis6991/treesitter.lua
+++ config/nvim/lua/lewis6991/treesitter.lua
@@ -1,8 +1,8 @@
 require'nvim-treesitter'.define_modules {
   fold = {
     attach = function()
-      vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
-      vim.opt.foldmethod = 'expr'
+      vim.opt_local.foldexpr = 'nvim_treesitter#foldexpr()'
+      vim.opt_local.foldmethod = 'expr'
       vim.cmd.normal'zx' -- recompute folds
     end,
     detach = function() end,
@@ -16,14 +16,16 @@ require'treesitter-context'.setup {
 }
 
 local langs = {
-    "python",
-    -- "json",
-    "html",
-    "bash",
-    "lua",
-    "rst",
-    -- "teal",
-  }
+  "bash",
+  "c",
+  "html",
+  "json",
+  "lua",
+  "make",
+  "python",
+  "rst",
+  "teal",
+}
 
 require'nvim-treesitter.configs'.setup {
   ensure_installed = langs,
@@ -49,45 +51,9 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
     disable = {'rst', 'make'}
   },
-  playground = { enable = true },
+  playground = { enable = true }, -- EXITFREE lag
   context_commentstring = { enable = true }
 }
 
--- local function get_node_at_line(root, lnum)
---   for node in root:iter_children() do
---     local srow, _, erow = node:range()
---     if srow == lnum then
---       return node
---     end
-
---     if node:child_count() > 0 and srow < lnum and lnum <= erow then
---       return get_node_at_line(node, lnum)
---     end
---   end
-
---   local wrapper = root:descendant_for_range(lnum, 0, lnum, -1)
---   local child = wrapper:child(0)
---   return child or wrapper
--- end
-
--- -- parsers.list.lua.install_info.url = '/Users/lewis/projects/nvim-tree-sitter-lua'
--- -- parsers.list.lua.install_info.url = '/Users/lewis/.data/nvim/site/pack/packer/start/nvim-treesitter/grammar/lua'
-
--- local function ts_get_tree(lnum)
---   lnum = lnum or vim.api.nvim_win_get_cursor(0)[1]
-
---   local parsers = require'nvim-treesitter.parsers'
---   local parser = parsers.get_parser()
-
---   if not parser or not lnum then
---     return -1
---   end
-
---   local node = get_node_at_line(parser:parse()[1]:root(), lnum-1)
---   while node do
---     print(vim.inspect(tostring(node)))
---     node = node:parent()
---   end
--- end
-
--- vim.api.nvim_set_keymap('n', '<leader>Z', '', {silent=true, callback=ts_get_tree})
+-- parsers.list.lua.install_info.url = '/Users/lewis/projects/nvim-tree-sitter-lua'
+-- parsers.list.lua.install_info.url = '/Users/lewis/.data/nvim/site/pack/packer/start/nvim-treesitter/grammar/lua'
