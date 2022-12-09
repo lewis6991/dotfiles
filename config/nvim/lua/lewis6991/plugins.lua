55982c82d8d877ddf06c523bc10ae4b19aaecf29
Lewis Russell <lewis6991@gmail.com>
2022-12-09 13:13:31 +0000

    remove semantic tokens plugin

--- config/nvim/lua/lewis6991/plugins.lua
+++ config/nvim/lua/lewis6991/plugins.lua
@@ -230,47 +230,6 @@ require('lewis6991.packer').setup {
     -- NavicSeparator
   end},
 
-  {'theHamsta/nvim-semantic-tokens', config = function()
-    local autocmd = require 'lewis6991.nvim'.autocmd
-
-    autocmd 'LspAttach' {
-      desc = 'semantic_tokens setup',
-      once = true,
-      function()
-        require("nvim-semantic-tokens").setup {
-          preset = "default",
-          -- highlighters is a list of modules following the interface of nvim-semantic-tokens.table-highlighter or
-          -- function with the signature: highlight_token(ctx, token, highlight) where
-          --        ctx (as defined in :h lsp-handler)
-          --        token  (as defined in :h vim.lsp.semantic_tokens.on_full())
-          --        highlight (a helper function that you can call (also multiple times) with the determined highlight group(s) as the only parameter)
-          highlighters = { require 'nvim-semantic-tokens.table-highlighter'}
-        }
-        vim.api.nvim_create_augroup('SemanticTokens', {})
-      end
-    }
-
-    autocmd 'LspAttach' {
-      desc = 'semantic_tokens attach',
-      function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        local bufnr = args.buf
-        local caps = client.server_capabilities
-        if caps.semanticTokensProvider and caps.semanticTokensProvider.full then
-          autocmd 'TextChanged' {
-            group = 'SemanticTokens',
-            buffer = bufnr,
-            function()
-              vim.lsp.buf.semantic_tokens_full()
-            end,
-          }
-          -- fire it first time on load as well
-          vim.lsp.buf.semantic_tokens_full()
-        end
-      end
-    }
-  end},
-
   {'ray-x/lsp_signature.nvim', config = function()
     require'lsp_signature'.setup{ hi_parameter = "Visual" }
   end},
