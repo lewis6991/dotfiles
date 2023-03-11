3b42a4a6b3de4c0a5f76c9e7b3da5a83fe1b44e8
Lewis Russell <lewis6991@gmail.com>
2023-03-11 08:48:39 +0000

    lua injections

--- config/nvim/queries/lua/injections.scm
+++ config/nvim/queries/lua/injections.scm
@@ -14,6 +14,18 @@
 
 (
   (function_call
+    (identifier) @_exec_lua
+    (arguments
+      (string) @lua)
+  )
+
+  (#eq? @_exec_lua "exec_lua")
+  (#lua-match? @lua "^%[=%[")
+  (#offset! @lua 0 3 0 -3)
+)
+
+(
+  (function_call
     (identifier) @_exec
     (arguments
       (string) @vim)
