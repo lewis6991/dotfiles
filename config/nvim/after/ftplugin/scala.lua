c62609588372085c7341db7284755f174c9ae6a7
Lewis Russell <lewis6991@gmail.com>
2023-05-31 17:03:45 +0100

    scala token

--- config/nvim/after/ftplugin/scala.lua
+++ config/nvim/after/ftplugin/scala.lua
@@ -5,3 +5,4 @@ vim.opt_local.foldlevelstart = 1
 vim.opt_local.foldnestmax    = 3
 
 vim.api.nvim_set_hl(0, '@type.scala', { default = true, link = '@class' })
+vim.api.nvim_set_hl(0, '@lsp.mod.readOnly.scala', { default = true, link = '@lsp' })
