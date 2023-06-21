1fd2cc884410d1b3cfac066be443b76094671a2b
Lewis Russell <lewis6991@gmail.com>
2023-06-21 14:29:07 +0100

    refactor: clipboard.lua

--- config/nvim/lua/lewis6991/clipboard.lua
+++ config/nvim/lua/lewis6991/clipboard.lua
@@ -1,17 +1,30 @@
-local api, fn = vim.api, vim.fn
-
 -- character table string
 local N = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 
 -- encoding
+--- @param data string
+--- @return string
 local function encode_base64(data)
-  return ((data:gsub('.', function(x)
-    local r, b = '', x:byte()
-    for i = 8, 1, -1 do
-      r = r .. (b % 2^i - b % 2^(i-1) > 0 and '1' or '0')
-    end
-    return r
-  end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+  local data1 = (
+    data:gsub(
+      '.',
+      --- @param x string
+      --- @return string
+      function(x)
+        local r, b = '', x:byte()
+        for i = 8, 1, -1 do
+          r = r .. (b % 2^i - b % 2^(i-1) > 0 and '1' or '0')
+        end
+        return r
+      end
+    )..'0000'
+  )
+
+  local data2 = data1:gsub(
+    '%d%d%d?%d?%d?%d?',
+    --- @param x string
+    --- @return string
+    function(x)
       if #x < 6 then
         return ''
       end
@@ -20,7 +33,12 @@ local function encode_base64(data)
         c = c + (x:sub(i, i) == '1' and 2^(6 - i) or 0)
       end
       return N:sub(c + 1, c + 1)
-    end)..({ '', '==', '=' })[#data%3+1])
+    end
+  )
+
+  local suffix = ({ '', '==', '=' })[#data%3+1]
+
+  return data2..suffix
 end
 
 local function osc52_copy(text)
@@ -29,9 +47,9 @@ local function osc52_copy(text)
   io.stderr:write(osc)
 end
 
-api.nvim_create_autocmd('TextYankPost', {
+vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
-    osc52_copy(fn.getreg(vim.v.event.regname))
+    osc52_copy(vim.fn.getreg(vim.v.event.regname))
   end
 })
 
