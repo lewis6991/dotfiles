d48f762d859a00c207952fcf39502ae730470c6f
Lewis Russell <lewis6991@gmail.com>
2023-07-12 12:07:56 +0100

    lazy load telescope

--- config/nvim/lua/lewis6991/telescope.lua
+++ config/nvim/lua/lewis6991/telescope.lua
@@ -1,42 +1,41 @@
-local telescope = require 'telescope'
 
-telescope.setup {
-  defaults = {
-    selection_strategy = "reset",
-    winblend = 15,
-  },
-  extensions = {
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown{}
+local did_setup = false
+
+local function setup()
+  if did_setup then
+    return
+  end
+  did_setup = true
+
+  local telescope = require 'telescope'
+
+  telescope.setup {
+    defaults = {
+      selection_strategy = "reset",
+      winblend = 15,
+    },
+    extensions = {
+      ["ui-select"] = {
+        require("telescope.themes").get_dropdown{}
+      }
     }
   }
-}
 
-telescope.load_extension('fzf')
+  telescope.load_extension('fzf')
+end
 
-local function nmap(key)
-  return function(spec)
-    local fun, opts
-    if type(spec) == 'string' then
-      fun = spec
-    else
-      fun = spec[1]
-      spec[1] = nil
-      opts = spec
-    end
-    opts = opts or {}
-    opts.preview = false
-    vim.keymap.set('n', key, function()
-      require('telescope.builtin')[fun](opts)
-    end, {})
-  end
+local function nmap(key, fun, opts)
+  vim.keymap.set('n', key, function()
+    setup()
+    require('telescope.builtin')[fun](opts)
+  end, {})
 end
 
 -- default: CTRL-B   scroll N screens Backwards
-nmap '<C-b>' 'buffers'
+nmap('<C-b>', 'buffers')
 
-nmap '<C-p>' {'git_files', use_git_root=true}
-nmap '<C- >' {'git_files', cwd="$HOME/projects/dotfiles", hidden=true}
+nmap('<C-p>', 'git_files', { use_git_root=true})
+nmap('<C- >', 'git_files', { cwd="$HOME/projects/dotfiles", hidden=true})
 
-nmap '<leader>f' 'find_files'
-nmap '<leader>g' 'live_grep'
+nmap('<leader>f', 'find_files')
+nmap('<leader>g', 'live_grep')
