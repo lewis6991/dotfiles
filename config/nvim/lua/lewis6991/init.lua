884e2c5a05de78344096015575042af82fc7a40e
Lewis Russell <lewis6991@gmail.com>
2023-07-31 10:45:01 +0100

    fixup: load lsp

--- config/nvim/lua/lewis6991/init.lua
+++ config/nvim/lua/lewis6991/init.lua
@@ -37,6 +37,7 @@ safe_require 'lewis6991.status'
 safe_require 'lewis6991.tabline'
 safe_require 'lewis6991.diagnostic'
 safe_require 'lewis6991.clipboard'
+safe_require 'lewis6991.lsp'
 safe_require 'lewis6991.ts_matchparen'
 
 local nvim = require 'lewis6991.nvim'
