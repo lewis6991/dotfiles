de5cc154ae82db4636005084cd5a0aa6326f04aa
Lewis Russell <lewis6991@gmail.com>
2023-08-08 12:03:05 +0100

    nvim update

--- config/nvim/lua/lewis6991/lsp.lua
+++ config/nvim/lua/lewis6991/lsp.lua
@@ -59,13 +59,10 @@ setup {
     if not vim.uv.fs_stat(path..'/.luarc.json') and not vim.uv.fs_stat(path..'/.luarc.jsonc') then
       local settings = vim.tbl_deep_extend('force', client.config.settings.Lua, {
         runtime = {
-          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
           version = 'LuaJIT'
         },
-        -- Make the server aware of Neovim runtime files
         workspace = {
           library = { vim.env.VIMRUNTIME }
-          -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
           -- library = vim.api.nvim_get_runtime_file("", true)
         }
       })
