1a3b2340787393d313dab410d5f083d4f0928476
Lewis Russell <lewis6991@gmail.com>
2024-03-07 10:59:21 +0000

    lsp document highlight

--- config/nvim/lua/lewis6991/lsp.lua
+++ config/nvim/lua/lewis6991/lsp.lua
@@ -1,21 +1,24 @@
+local api, lsp = vim.api, vim.lsp
+
 local lspconfig = require('lspconfig')
 
+--- @class LspClientConfig : vim.lsp.ClientConfig
+--- @field cmd? string[]
+
+--- @param name string
+--- @param user_config? LspClientConfig
 local function setup(name, user_config)
   user_config = user_config or {}
-  user_config.on_setup = function(config)
-    config.capabilities = vim.lsp.protocol.make_client_capabilities()
-    vim.tbl_extend('force', config.capabilities, require('cmp_nvim_lsp').default_capabilities())
-  end
-
+  local capabilities = lsp.protocol.make_client_capabilities()
+  user_config.capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
   lspconfig[name].setup(user_config)
 end
 
---- @param client lsp.Client
+--- @param client vim.lsp.Client
 --- @param settings table
 local function add_settings(client, settings)
-  local config = client.config
-  config.settings = vim.tbl_deep_extend('force', config.settings, settings)
-  client.notify('workspace/didChangeConfiguration', { settings = config.settings })
+  client.settings = vim.tbl_deep_extend('force', client.settings, settings)
+  client.notify('workspace/didChangeConfiguration', { settings = client.settings })
 end
 
 local function default_lua_settings()
@@ -31,7 +34,7 @@ local function default_lua_settings()
           '${3rd}/busted/library',
           '${3rd}/luv/library',
         },
-        -- library = vim.api.nvim_get_runtime_file("", true)
+        -- library = api.nvim_get_runtime_file("", true)
       },
     },
   }
@@ -51,7 +54,6 @@ setup('lua_ls', {
     then
       add_settings(client, default_lua_settings())
     end
-    return true
   end,
   settings = {
     Lua = {
@@ -68,26 +70,54 @@ setup('pyright')
 setup('ruff_lsp')
 setup('bashls')
 
+local function debounce(ms, fn)
+  local timer = assert(vim.uv.new_timer())
+  return function(...)
+    local argc, argv = select('#', ...), { ... }
+    timer:start(ms, 0, function()
+      timer:stop()
+      vim.schedule(function()
+        fn(unpack(argv, 1, argc))
+      end)
+    end)
+  end
+end
+
 -- install with:
 --   npm install -g vscode-langservers-extracted
 -- setup('jsonls')
 
-vim.api.nvim_create_autocmd('LspAttach', {
+api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    local client = assert(lsp.get_client_by_id(args.data.client_id))
+
     if client.supports_method('textDocument/codeLens') then
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'InsertLeave' }, {
+      api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'InsertLeave' }, {
         buffer = args.buf,
-        callback = vim.lsp.codelens.refresh,
+        callback = lsp.codelens.refresh,
       })
-      vim.lsp.codelens.refresh()
+      lsp.codelens.refresh()
+    end
+
+    if client.supports_method('textDocument/documentHighlight') then
+      api.nvim_create_autocmd({ 'FocusGained', 'WinEnter', 'BufEnter', 'CursorMoved', 'CursorHold', 'CursorHoldI' }, {
+        buffer = args.buf,
+        callback = debounce(200, function()
+          lsp.buf.clear_references()
+          lsp.buf.document_highlight()
+        end)
+      })
+      api.nvim_create_autocmd({ 'FocusLost', 'WinLeave', 'BufLeave' }, {
+        buffer = args.buf,
+        callback = lsp.buf.clear_references
+      })
     end
   end,
 })
 
--- vim.api.nvim_create_autocmd('LspAttach', {
+-- api.nvim_create_autocmd('LspAttach', {
 --   callback = function(args)
---     local client = vim.lsp.get_client_by_id(args.data.client_id)
+--     local client = lsp.get_client_by_id(args.data.client_id)
 --     client.server_capabilities.semanticTokensProvider = nil
 --   end
 -- })
