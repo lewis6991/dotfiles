87837a9ee9f9d66361931fa407c3102354edaf3d
Lewis Russell <lewis6991@gmail.com>
2024-03-11 10:04:56 +0000

    update gitsigns

--- config/nvim/lua/lewis6991/gitsigns.lua
+++ config/nvim/lua/lewis6991/gitsigns.lua
@@ -2,17 +2,8 @@ local gitsigns = require('gitsigns')
 
 local line = vim.fn.line
 
---  TODO(lewis6991): doesn't work properly
 vim.keymap.set('n', 'M', '<cmd>Gitsigns debug_messages<cr>')
 vim.keymap.set('n', 'mm', '<cmd>Gitsigns dump_cache<cr>')
-
-local function wrap(f, ...)
-  local args = { ... }
-  local nargs = select('#', ...)
-  return function()
-    f(unpack(args, 1, nargs))
-  end
-end
 
 local function on_attach(bufnr)
   local function map(mode, l, r, opts)
@@ -39,21 +30,32 @@ local function on_attach(bufnr)
 
   map('n', '<leader>hs', gitsigns.stage_hunk)
   map('n', '<leader>hr', gitsigns.reset_hunk)
+
   map('v', '<leader>hs', function()
     gitsigns.stage_hunk({ line('.'), line('v') })
   end)
+
   map('v', '<leader>hr', function()
     gitsigns.reset_hunk({ line('.'), line('v') })
   end)
+
   map('n', '<leader>hS', gitsigns.stage_buffer)
   map('n', '<leader>hu', gitsigns.undo_stage_hunk)
   map('n', '<leader>hR', gitsigns.reset_buffer)
   map('n', '<leader>hp', gitsigns.preview_hunk)
-  map('n', '<leader>hi', gitsigns.preview_hunk_inline)
-  map('n', '<leader>hb', wrap(gitsigns.blame_line, { full = true }))
+
+  map('n', '<leader>hb', function()
+    gitsigns.blame_line({full=true})
+  end)
+
   map('n', '<leader>hi', gitsigns.preview_hunk_inline)
   map('n', '<leader>hd', gitsigns.diffthis)
   map('n', '<leader>hD', ':Gitsigns diffthis ~')
+
+  map('n', '<leader>hld', function()
+    gitsigns.diffthis(vim.b.gitsigns_blame_line_dict.sha..'~1')
+  end)
+
   map('n', '<leader>hB', ':Gitsigns change_base ~')
 
   -- Toggles
@@ -61,7 +63,7 @@ local function on_attach(bufnr)
   map('n', '<leader>td', gitsigns.toggle_deleted)
   map('n', '<leader>tw', gitsigns.toggle_word_diff)
 
-  map('n', '<leader>hQ', wrap(gitsigns.setqflist, 'all'))
+  map('n', '<leader>hQ', function() gitsigns.setqflist('all') end)
   map('n', '<leader>hq', gitsigns.setqflist)
 
   map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
