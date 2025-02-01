30057fc88a9ab5653d94221b641ccc809f39d14d
Lewis Russell <lewis6991@gmail.com>
2025-02-01 13:36:22 +0000

    update auto_lua_require

--- config/nvim/lua/lewis6991/lsp/auto_lua_require.lua
+++ config/nvim/lua/lewis6991/lsp/auto_lua_require.lua
@@ -5,20 +5,24 @@ local api = vim.api
 local function match_require(x)
   return x:match('require')
     and (
-      x:match("require%s*%(%s*'([^.']+).*'%)")
-      or x:match('require%s*%(%s*"([^."]+).*"%)')
-      or x:match("require%s*'([^.']+).*'%)")
-      or x:match('require%s*"([^."]+).*"%)')
+      x:match("require%s*%(%s*'([^.']+).*'%)") -- require('<module>')
+      or x:match('require%s*%(%s*"([^."]+).*"%)') -- require("<module>")
+      or x:match("require%s*'([^.']+).*'%)") -- require '<module>'
+      or x:match('require%s*"([^."]+).*"%)') -- require "<module>"
+      or x:match("pcall(require,%s*'([^.']+).*'%)") -- pcall(require, "<module>")
+      or x:match('pcall(require,%s*"([^."]+).*"%)') -- pcall(require, "<module>")
     )
 end
 
 --- @param client vim.lsp.Client
 --- @param bufnr integer
 return function(client, bufnr)
+  local local_ws = nil
   if client.workspace_folders then
     local path = client.workspace_folders[1].name
+    local_ws = vim.fs.joinpath(path, 'lua')
     if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
-      -- Updates to settings are ingored if a .luarc.json is present
+      -- Updates to settings are ignored if a .luarc.json is present
       return
     end
   end
@@ -29,8 +33,8 @@ return function(client, bufnr)
 
   --- @param first? integer
   --- @param last? integer
-  local function on_lines(first, last)
-    local do_change = false
+  local function on_lines(_, _, _, first, _, last)
+    local did_change = false
 
     local lines = api.nvim_buf_get_lines(bufnr, first or 0, last or -1, false)
     for _, line in ipairs(lines) do
@@ -39,26 +43,22 @@ return function(client, bufnr)
         for _, mod in ipairs(vim.loader.find(m, { patterns = { '', '.lua' } })) do
           local lib = vim.fs.dirname(mod.modpath)
           local libs = client.settings.Lua.workspace.library
-          if not vim.tbl_contains(libs, lib) then
+          if not lib == local_ws and not vim.tbl_contains(libs, lib) then
             libs[#libs + 1] = lib
-            do_change = true
+            did_change = true
           end
         end
       end
     end
 
-    if do_change then
-      client.notify('workspace/didChangeConfiguration', { settings = client.settings })
+    if did_change then
+      client:notify('workspace/didChangeConfiguration', { settings = client.settings })
     end
   end
 
   api.nvim_buf_attach(bufnr, false, {
-    on_lines = function(_, _, _, first, _, last)
-      on_lines(first, last)
-    end,
-    on_reload = function()
-      on_lines()
-    end,
+    on_lines = on_lines,
+    on_reload = on_lines,
   })
 
   -- Initial scan
